c07626de284b25e41678c2775442b48de8375181
Collins Gikungu <gikungu2002@gmail.com>
2026-09-15 22:06:32 +0300

    Update build artifacts and assembly information for ServiceFlow.API

--- obj/Debug/net8.0/ServiceFlow.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/ServiceFlow.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceFlow.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ff021050d77ceb113f5d7cf762fe566cda96c30")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceFlow.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceFlow.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
